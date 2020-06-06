511415bf0c87feba859efb578427956970899873
7shi <7shi@live.jp>
2020-06-06 15:14:11 +0900

    use ProcessPoolExecutor

--- python/etc/size-parallel.py
+++ python/etc/size-parallel.py
@@ -1,50 +1,38 @@
-import bz2, concurrent.futures, io, sys, threading
+import bz2, io
 
-para = 1
-if len(sys.argv) > 1:
-    para = min(256, max(1, int(sys.argv[1])))
+def getlength(args):
+    target, pos, length = args
+    with open(target, "rb") as f:
+        f.seek(pos)
+        bz2data = f.read(length)
+    data  = bz2.decompress(bz2data)
+    text  = data.decode("utf-8")
+    lines = 0
+    with io.StringIO(text) as f:
+        while (_ := f.readline()):
+            lines  += 1
+    return { "bytes": len(data), "chars": len(text), "lines": lines }
 
-slen = []
-with open("streamlen.tsv") as f:
-    target = f.readline().strip()
-    while (line := f.readline()):
-        slen.append(int(line))
+if __name__ == "__main__":
+    import concurrent.futures
 
-with open(target, "rb") as f:
-    lock = threading.Lock()
-    def getstream():
-        lock.acquire()
-        ret = f.read(slen.pop(0)) if slen else None
-        lock.release()
-        return ret
+    spos, slen = [], []
+    with open("streamlen.tsv") as f:
+        target = f.readline().strip()
+        pos = 0
+        while (line := f.readline()):
+            length = int(line)
+            spos.append(pos)
+            slen.append(length)
+            pos += length
+    split = 10
+    poslens = [(target, spos[i], sum(slen[i : i + split]))
+               for i in range(0, len(slen), split)]
 
-    def getlength():
-        streams = 0
-        bytes   = 0
-        chars   = 0
-        lines   = 0
-        while (bz2data := getstream()):
-            data = bz2.decompress(bz2data)
-            text = data.decode("utf-8")
-            streams += 1
-            bytes   += len(data)
-            chars   += len(text)
-            with io.StringIO(text) as f:
-                while (line := f.readline()):
-                    lines  += 1
-        return {"streams": streams,
-                "bytes"  : bytes,
-                "chars"  : chars,
-                "lines"  : lines}
-
-    executor = concurrent.futures.ThreadPoolExecutor()
-    futures = [executor.submit(getlength) for _ in range(para)]
-    results = [future.result() for future in futures]
-    executor.shutdown()
-    print(results)
-    sums = {}
-    for r in results:
-        for k, v in r.items():
-            if not k in sums: sums[k] = 0
-            sums[k] += v
+    sums = { "streams": len(spos) }
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for result in executor.map(getlength, poslens):
+            for k, v in result.items():
+                if not k in sums: sums[k] = 0
+                sums[k] += v
     print(", ".join([f"{k}: {v:,}" for k, v in sums.items()]))
