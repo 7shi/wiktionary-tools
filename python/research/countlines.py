4104d5cec72ceb703d62f3d6bfcc51efdc05d81c
7shi <7shi@live.jp>
2020-06-12 04:54:38 +0900

    modified:   python/research/countlines.py

--- python/research/countlines.py
+++ python/research/countlines.py
@@ -2,6 +2,6 @@ import bz2
 target = "enwiktionary-20200501-pages-articles-multistream.xml.bz2"
 lines = 0
 with bz2.open(target, "rt", encoding="utf-8") as f:
-    while (_ := f.readline()):
+    while (line := f.readline()):
         lines += 1
 print(f"lines: {lines:,}")
