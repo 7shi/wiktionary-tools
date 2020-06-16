88d5b82ff196397fb0922399f6ae3b01d43bb666
7shi <7shi@live.jp>
2020-06-16 19:47:29 +0900

    modified:   python/tools/rank.sql

--- python/tools/rank.sql
+++ python/tools/rank.sql
@@ -1,3 +1,5 @@
+.mode ascii
+.separator "\t" "\n"
 SELECT RANK() OVER(ORDER BY cnt DESC) AS rank, name, cnt FROM langname
 INNER JOIN (
     SELECT lid, COUNT(lid) as cnt FROM idlang GROUP BY lid ORDER BY cnt DESC
